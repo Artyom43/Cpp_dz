2074372d980d6aba8d65c84ad01e8fe92e737c77
Artyom43 <48893080+Artyom43@users.noreply.github.com>
2019-10-13 20:30:44 +0300

    Update Graph_bypass.cpp

--- Graph_bypass.cpp
+++ Graph_bypass.cpp
@@ -5,12 +5,12 @@ typedef size_t Vertex;
 
 class Graph{
 public:
-    Graph(size_t vertex_count, bool is_directed = 0) {
+    Graph(Vertex vertex_count, bool is_directed = 0) {
         vertex_count_ = vertex_count;
         is_directed_ = is_directed;
     }
 
-    size_t getVertexCount() const {
+    Vertex getVertexCount() const {
         return vertex_count_;
     }
     bool getIsDirected() const {
@@ -18,7 +18,7 @@ public:
     }
 
 protected:
-    size_t vertex_count_;
+    Vertex vertex_count_;
     bool is_directed_;
 
 };
@@ -26,11 +26,11 @@ protected:
 
 class GraphAdjList: public Graph {
 public:
-    GraphAdjList(size_t vertex_count, size_t is_directed, const std::vector<std::vector<size_t>>& edge_matrix)
+    GraphAdjList(Vertex vertex_count, bool is_directed, const std::vector<std::vector<Vertex>>& edge_matrix)
         :Graph(vertex_count, is_directed) {
-            edge_list_ = std::vector<std::vector<size_t>>(vertex_count);
-            for (size_t i = 0; i < vertex_count_; ++i) {
-                for (size_t j = 0; j < vertex_count_; ++j) {
+            edge_list_ = std::vector<std::vector<Vertex>>(vertex_count);
+            for (Vertex i = 0; i < vertex_count_; ++i) {
+                for (Vertex j = 0; j < vertex_count_; ++j) {
                     if (edge_matrix[i][j] == 1) {
                         edge_list_[i].push_back(j);
                     }
@@ -38,12 +38,16 @@ public:
             }
     }
 
-    std::vector<std::vector<size_t>> getEdgeList() const {
+    std::vector<Vertex> getNeighbours(Vertex v) const {
+        return edge_list_.at(v);
+    }
+
+    std::vector<std::vector<Vertex>> getEdgeList() const {
         return edge_list_;
     }
 
 private:
-    std::vector<std::vector<size_t>> edge_list_;
+    std::vector<std::vector<Vertex>> edge_list_;
 };
 
 
@@ -53,21 +57,20 @@ namespace GraphProcessing{
 
     std::vector<VertexMark> dfsVisitV(const GraphAdjList& g, Vertex v, std::vector<VertexMark>& color) {
         color[v] = GRAY;
-        for (size_t i = 0; i < g.getEdgeList()[v].size(); ++i) {
-            size_t v_neighbor = g.getEdgeList()[v][i];
-            if (color[v_neighbor] == WHITE) {
-                dfsVisitV(g, v_neighbor, color);
+        for (u : g.getNeighbors(v)) {
+            if (color[u] == WHITE) {
+                dfsVisitV(g, u, color);
             }
         }
         color[v] = BLACK;
         return color;
     }
 
-    size_t getAdjVertexNumber (const GraphAdjList& g, Vertex v) {
+    Vertex getCountOfVertexInOneComponent(const GraphAdjList& g, Vertex v) {
         size_t result = 0;
         std::vector<VertexMark> color = std::vector<VertexMark>{g.getVertexCount(), WHITE};
         dfsVisitV(g, v, color);
-        for (size_t v = 0; v < g.getVertexCount(); ++v) {
+        for (Vertex v = 0; v < g.getVertexCount(); ++v) {
             if (color[v] == BLACK) {
                 ++result;
             }
@@ -79,19 +82,19 @@ namespace GraphProcessing{
 
 
 int main() {
-    size_t vertex_number, the_vertex;
+    Vertex vertex_number, the_vertex;
     std::cin >> vertex_number >> the_vertex;
 
-    std::vector<std::vector<size_t>> edge_matrix;
+    std::vector<std::vector<Vertex>> edge_matrix;
     for (int i = 0; i < vertex_number; ++i) {
-        edge_matrix.push_back(std::vector<size_t>());
+        edge_matrix.push_back(std::vector<Vertex>());
         for (int j = 0; j < vertex_number; ++j) {
-            size_t have_edge;
+            Vertex have_edge;
             std::cin >> have_edge;
             edge_matrix[i].push_back(have_edge);
         }
     }
     GraphAdjList g(vertex_number, false, edge_matrix);
-    std::cout << GraphProcessing::getAdjVertexNumber(g, --the_vertex);
+    std::cout << GraphProcessing::getCountOfVertexInOneComponent(g, --the_vertex);
     return 0;
 }
