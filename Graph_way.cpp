2f70df411f97d2f29828d7cbb305bf9a1ff17b7f
Artyom43 <48893080+Artyom43@users.noreply.github.com>
2019-10-15 19:12:43 +0300

    Update Graph_way.cpp

--- Graph_way.cpp
+++ Graph_way.cpp
@@ -13,14 +13,20 @@ public:
         is_directed_ = is_directed;
     }
 
+    virtual std::vector<size_t> getNeighbors(Vertex v) const = 0;
+
     size_t getVertexCount() const {
         return vertex_count_;
     }
     bool getIsDirected() const {
         return is_directed_;
     }
+    size_t getEdgeCount() const {
+        return edge_count_;
+    }
 
 protected:
+    size_t edge_count_;
     size_t vertex_count_;
     bool is_directed_;
 
@@ -32,28 +38,40 @@ protected:
 
 class GraphAdjList: public Graph {
 public:
-    GraphAdjList(size_t vertex_count, size_t is_directed, const std::vector<std::vector<size_t>>& edge_matrix)
+    GraphAdjList(size_t vertex_count, const std::vector<std::vector<size_t>>& edge_matrix, bool is_directed = false)
         :Graph(vertex_count, is_directed) {
-            edge_list_ = std::vector<std::vector<size_t>>(vertex_count);
+            if (edge_matrix.size() != vertex_count) {
+                throw std::runtime_error("wrong matrix size");
+            }
+            for (int i = 0; i < vertex_count; ++i) {
+                if (edge_matrix[i].size() != vertex_count) {
+                    throw std::runtime_error("wrong time error");
+                }
+            }
+            edge_count_ = 0;
+            vertex_count_ = vertex_count;
+            edge_list_ = std::vector<std::vector<Vertex>>(vertex_count);
             for (size_t i = 0; i < vertex_count_; ++i) {
                 for (size_t j = 0; j < vertex_count_; ++j) {
                     if (edge_matrix[i][j] == 1) {
+                        ++edge_count_;
                         edge_list_[i].push_back(j);
                     }
                 }
             }
-    }
+            edge_count_ /= 2;
+        }
 
-    std::vector<size_t> getNeighbors(Vertex v) const {
+    std::vector<Vertex> getNeighbors(Vertex v) const override{
         return edge_list_[v];
     }
 
-    std::vector<std::vector<size_t>> getEdgeList() const {
+    std::vector<std::vector<Vertex>> getEdgeList() const {
         return edge_list_;
     }
 
 private:
-    std::vector<std::vector<size_t>> edge_list_;
+    std::vector<std::vector<Vertex>> edge_list_;
 };
 
 
@@ -64,77 +82,46 @@ namespace GraphProcessing{
 
     enum VertexMark {WHITE, BLACK, GRAY};
 
-    std::vector<VertexMark> dfsVisitV(const GraphAdjList& g, Vertex v, std::vector<VertexMark>& color) {
-        color[v] = GRAY;
-        for (size_t i = 0; i < g.getEdgeList()[v].size(); ++i) {
-            size_t v_neighbor = g.getEdgeList()[v][i];
-            if (color[v_neighbor] == WHITE) {
-                dfsVisitV(g, v_neighbor, color);
-            }
-        }
-        color[v] = BLACK;
-        return color;
-    }
+    const int no_way = -1;
 
-    size_t getAdjVertexNumber (const GraphAdjList& g, Vertex v) {
-        size_t result = 0;
-        std::vector<VertexMark> color = std::vector<VertexMark>{g.getVertexCount(), WHITE};
-        dfsVisitV(g, v, color);
-        for (size_t i = 0; i < g.getVertexCount(); ++i) {
-            if (color[i] == BLACK) {
-                ++result;
-            }
-        }
-        return result;
-    }
-
-    std::vector<int> bfs(const GraphAdjList& g, Vertex v) {
-        std::vector<int> distances(g.getVertexCount(), -1);
+    std::pair<std::vector<int>, std::vector<std::vector<Vertex>>> bfs(const Graph& g, Vertex v) {
+        std::vector<int> distances(g.getVertexCount(), no_way);
         std::vector<VertexMark> color(g.getVertexCount(), WHITE);
-        std::queue<size_t> cur_vertexes;
+        std::queue<Vertex> cur_vertexes;
+        std::vector<std::vector<Vertex>> prevs(g.getVertexCount());
         distances[v] = 0;
         cur_vertexes.push(v);
         color[v] = GRAY;
         while (!cur_vertexes.empty()) {
-            size_t v = cur_vertexes.front();
+            Vertex v = cur_vertexes.front();
             cur_vertexes.pop();
-            for (size_t i = 0; i < g.getNeighbors(v).size(); ++i) {
-                Vertex u = g.getNeighbors(v)[i];
+            for (Vertex u : g.getNeighbors(v)) {
                 if (color[u] == WHITE) {
+                    prevs[u].push_back(v);
                     distances[u] = distances[v] + 1;
                     color[u] = GRAY;
                     cur_vertexes.push(u);
                 }
             }
         }
-        return distances;
+        return std::make_pair(distances, prevs);
     }
 
-    void findShortestWay(const GraphAdjList& g, int cur_distance, std::vector<size_t>& way,
-                                         Vertex u, const std::vector<int>& distances) {
-        --cur_distance;
-        for (int i = 0; i < g.getNeighbors(u).size(); ++i) {
-            Vertex neighbor = g.getNeighbors(u)[i];
-            if (cur_distance < 0) {
-                return;
-            }
-            if (distances[neighbor] == cur_distance) {
-                way.push_back(neighbor + 1);
-                findShortestWay(g, cur_distance, way, neighbor, distances);
-                break;
-            }
-        }
-    }
-
-    std::pair<int, std::vector<size_t>> shortestWayFromVToU(const GraphAdjList& g, Vertex v, Vertex u) {
-        --u;
-        --v;
-        std::vector<int> distances = bfs(g, v);
+    std::vector<Vertex> shortestWay(const Graph& g, Vertex v, Vertex u) {
+        auto bfs_return = bfs(g, v);
+        std::vector<int> distances = bfs_return.first;
+        std::vector<std::vector<Vertex>> prevs = bfs_return.second;
         int min_distance = distances[u];
-        std::vector<size_t> way;
-        way.push_back(u + 1);
-        findShortestWay(g, min_distance, way, u, distances);
-        return std::make_pair(min_distance, way);
+        if (min_distance == no_way) {
+            return std::vector<Vertex>(0);
+        }
+        std::vector<Vertex> way;
+        way.push_back(u);
+        while (u != v) {
+            way.push_back(prevs[u][0]);
+            u = prevs[u][0];
+        }
+        return way;
     }
 
 }
@@ -159,18 +146,22 @@ int main() {
 
     Vertex v, u;
     std::cin >> v >> u;
+    --v; --u;
 
-    GraphAdjList g(num_vertexes, false, edge_matrix);
+    GraphAdjList g(num_vertexes, edge_matrix);
 
-    std::pair<int, std::vector<size_t>> result = GraphProcessing::shortestWayFromVToU(g, v, u);
-    int min_distance = result.first;
-    std::vector<size_t> way = result.second;
-    std::cout << min_distance << "\n";
-    if (min_distance == -1 || min_distance == 0) {
+    std::vector<size_t> way = GraphProcessing::shortestWay(g, v, u);
+    if (way.size() == 0) {
+        std::cout << -1;
         return 0;
     }
+    if (way.size() == 1) {
+        std::cout << 0;
+        return 0;
+    }
+    std::cout << (way.size() - 1) << "\n";
     for (int i = way.size() - 1; i >= 0; --i) {
-        std::cout << way[i] << " ";
+        std::cout << (way[i] + 1) << " ";
     }
     return 0;
 }
