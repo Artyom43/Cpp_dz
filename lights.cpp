8cf56dc7539169f18e38cb0b36428190e809d610
Artyom43 <48893080+Artyom43@users.noreply.github.com>
2019-10-15 17:41:03 +0300

    Update lights.cpp

--- lights.cpp
+++ lights.cpp
@@ -22,23 +22,27 @@ public:
         if (edge_matrix.size() != vertex_count) {
             throw std::runtime_error("wrong matrix size");
         }
-        for (int i =0; i < vertex_count; ++i) {
-            if (edge_matrix_[i].size() != vertex_count) {
+        for (int i = 0; i < vertex_count; ++i) {
+            if (edge_matrix[i].size() != vertex_count) {
                 throw std::runtime_error("wrong time error");
             }
         }
+        edge_count_ = 0;
         vertex_count_ = vertex_count;
-        edge_matrix_ = edge_matrix;
+        edge_matrix_ = std::vector<std::vector<int>> (vertex_count_, std::vector<int>(vertex_count_));
+        for (size_t i = 0; i < vertex_count_; ++i) {
+            for (size_t j = 0; j < vertex_count_; ++j) {
+                if (edge_matrix[i][j] == 1) {
+                    ++edge_count_;
+                }
+                edge_matrix_[i][j] = edge_matrix[i][j];
+            }
+        }
+        edge_count_ /= 2;
     }
 
     size_t getEdgeCount() {
-        int result = 0;
-        for (Vertex i = 0; i < vertex_count_; ++i) {
-            for (Vertex j = 0; j < vertex_count_; ++j) {
-                result += edge_matrix_[i][j];
-            }
-        }
-        return result / 2;
+        return edge_count_;
     }
 
 
